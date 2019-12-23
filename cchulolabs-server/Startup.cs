0d6ab8cd20bb9e15ba3c6b4be338ffba47177ba8
Carlos Chulo <thecompscientist.dev@gmail.com>
2019-12-23 13:54:26 -0800

    cleaned up Startup.cs a little more, removing dead code. Upgrading the client to the latest version of Angular Material

--- cchulolabs-server/Startup.cs
+++ cchulolabs-server/Startup.cs
@@ -34,12 +34,7 @@ namespace cchulolabs.server {
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env) {
             if (env.IsDevelopment()) {
                 app.UseDeveloperExceptionPage();
-            } else {
-                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-                app.UseHsts();
             }
-
-            
 
             app.UseHttpsRedirection();
 
