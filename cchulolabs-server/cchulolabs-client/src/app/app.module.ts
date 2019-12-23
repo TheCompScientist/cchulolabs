18c01692afb09e9fa85fcb9f534cabb40a7e00e7
Carlos Chulo <thecompscientist.dev@gmail.com>
2019-12-23 15:08:19 -0800

    Created a core module to house all common modules for the site

--- cchulolabs-server/cchulolabs-client/src/app/app.module.ts
+++ cchulolabs-server/cchulolabs-client/src/app/app.module.ts
@@ -4,7 +4,7 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { AngularMaterialModule } from './angular-material/angular-material.module';
+import { CoreModule } from './core/core.module';
 
 @NgModule({
   declarations: [
@@ -14,7 +14,7 @@ import { AngularMaterialModule } from './angular-material/angular-material.modul
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    AngularMaterialModule
+    CoreModule
   ],
   providers: [],
   bootstrap: [AppComponent]
