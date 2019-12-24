986db3ce329afc01cd2cf29edf4f9f1981ebcba8
Carlos Chulo <thecompscientist.dev@gmail.com>
2019-12-23 17:13:06 -0800

    the frontend is starting to come together

--- cchulolabs-server/cchulolabs-client/src/app/app.component.ts
+++ cchulolabs-server/cchulolabs-client/src/app/app.component.ts
@@ -1,9 +1,20 @@
 import { Component } from '@angular/core';
 
+interface ILink {
+  label: string;
+}
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
+  links: Array<ILink> = [
+    { label: 'Home' },
+    { label: 'About' },
+    { label: 'Blog' },
+    { label: 'Projects' },
+    { label: 'Resume' }
+  ];
 }
