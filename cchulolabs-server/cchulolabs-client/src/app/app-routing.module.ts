976e24052ef190f4c0fa150f264a0a97a5de9a1b
Carlos Chulo <thecompscientist.dev@gmail.com>
2019-12-23 19:03:44 -0800

    Set the typography for the application by making use of Mat-Typography and wrapping it on app.component level. Linked all the pages to app.module

--- cchulolabs-server/cchulolabs-client/src/app/app-routing.module.ts
+++ cchulolabs-server/cchulolabs-client/src/app/app-routing.module.ts
@@ -1,8 +1,34 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './pages/components/home/home.component';
+import { AboutComponent } from './pages/components/about/about.component';
+import { BlogComponent } from './pages/components/blog/blog.component';
+import { ProjectsComponent } from './pages/components/projects/projects.component';
+import { ResumeComponent } from './pages/components/resume/resume.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: HomeComponent
+  },
+  {
+    path: 'about',
+    component: AboutComponent
+  },
+  {
+    path: 'blog',
+    component: BlogComponent
+  },
+  {
+    path: 'projects',
+    component: ProjectsComponent
+  },
+  {
+    path: 'resume',
+    component: ResumeComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- cchulolabs-server/cchulolabs-client/src/app/pages/pages.module.ts
+++ cchulolabs-server/cchulolabs-client/src/app/pages/pages.module.ts
@@ -1,13 +1,32 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HomeComponent } from './components/home/home.component';
+import { AboutComponent } from './components/about/about.component';
+import { BlogComponent } from './components/blog/blog.component';
+import { ProjectsComponent } from './components/projects/projects.component';
+import { ResumeComponent } from './components/resume/resume.component';
+import { CoreModule } from '../core/core.module';
 
 
 
 @NgModule({
-  declarations: [HomeComponent],
+  declarations: [
+    HomeComponent,
+    AboutComponent,
+    BlogComponent,
+    ProjectsComponent,
+    ResumeComponent
+  ],
   imports: [
-    CommonModule
+    CommonModule,
+    CoreModule
+  ],
+  exports: [
+    HomeComponent,
+    AboutComponent,
+    BlogComponent,
+    ProjectsComponent,
+    ResumeComponent
   ]
 })
 export class PagesModule { }
